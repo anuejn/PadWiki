d144466ab4b1501c8af92513bb44b892f86952e8
jaro <jarohabiger@googleamil.com>
2017-04-26 18:19:51 +0200

    [WIP] wireframe markup

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
-ep_base
-=======
+# PadWiki
+A simple sidebar for etherpad lite which adds wiki-like functionality.
 
-A sample etherpad-lite plugin
-
-# notes
-    replace all CAPITALIZED names with whatever you want
-
-# installing
-
-    npm install PATH/TO/THIS/FOLDER (from your etherpad-lite folder)
+## Install it
--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-exports.FUNCTIONNAME1 = function(hook, context){
+// the express server hook 
+exports.expressCreateServer = function (hook_name, args, cb) {
+  args.app.get('/w/*', function(req, res) {
+    res.sendFile(__dirname + "/static/wiki/wiki_main.html");
+  });
+}
+
+// handle the socketio traffic for the sidebar
+exports.socketio = function() {
+
 }
